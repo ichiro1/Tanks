6e721e1a780277dbdb43b19fc41183a62c09c784
UCode <ucode@UCODE-TPV-ZETA.fios-router.home>
2018-06-17 15:58:26 -0700

    Shot Shells

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -39,11 +39,34 @@ public class TankShooting : MonoBehaviour
     private void Update()
     {
         // Track the current state of the fire button and make decisions based on the current launch force.
-    }
+		m_AimSlider.value = m_CurrentLaunchForce;
+		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
+			m_CurrentLaunchForce = m_MaxLaunchForce;
+			Fire ();
+		} else if (Input.GetButtonDown (m_FireButton)) {
+			m_Fired = false;
+			m_CurrentLaunchForce = m_MinLaunchForce;
+			m_ShootingAudio.clip = m_ChargingClip;
+			m_ShootingAudio.Play ();
+		}
+		else if (Input.GetButton (m_FireButton) && !m_Fired) {
+			m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+		}
+		else if (Input.GetButtonUp (m_FireButton) && !m_Fired) {
+			Fire ();
+		}
+	}
 
 
     private void Fire()
     {
         // Instantiate and launch the shell.
+		m_Fired = true;
+		Rigidbody shellInstance = Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+		shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+		m_ShootingAudio.clip = m_FireClip;
+		m_ShootingAudio.Play();
+		m_CurrentLaunchForce = m_MinLaunchForce;
     }
 }
